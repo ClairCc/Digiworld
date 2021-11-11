17fe6f1256a8bdad7767c5ef5c8c4016947fcfb2
Cristian Cavanzo <cavanzoariasc@gmail.com>
2021-11-11 14:36:27 -0500

    Configurando las variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,33 +1,38 @@
-const { Sequelize } = require('sequelize');
-const path = require('path');
-const dotenv = require('dotenv');
+const { Sequelize } = require("sequelize");
+const path = require("path");
+const dotenv = require("dotenv");
 
 dotenv.config({
-    path: path.resolve(__dirname, '../development.env')
+  path: path.resolve(__dirname, "../development.env"),
 });
 
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+console.log(process.env.DB_DATABASE);
 
 const db = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    dialectOptions: {
-        charset: 'utf8_general_ci',
-        ssl: { rejectUnauthorized: false },
-    },
-    define: {
-        timestamps: false
-    },
-    // pool: {
-    //     max: 5,
-    //     min: 0,
-    //     acquire: 30000,
-    //     idle: 10000
-    // },
-    logging: false
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  dialectOptions: {
+    charset: "utf8_general_ci",
+    // ssl: { rejectUnauthorized: false },
+  },
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  logging: false,
 });
 
 // db.authenticate().then(
